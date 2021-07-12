c2f014d8119efc28f8eb4429c698eba3b627b303
yijun.liu <francislyj@gmail.com>
2021-07-12 18:49:34 +0800

    add concurrency

--- concurrency.go
+++ concurrency.go
@@ -13,16 +13,41 @@ func print(n int) {
 		fmt.Println("randomNum :", randomNum)
 		time.Sleep(time.Millisecond * randomNum)
 	}
+
+
+}
+
+func hello(data string, channelCall chan string) {
+	fmt.Println("channel called")
+	data = data + "hello"
+	fmt.Println("data changed")
+
+	fmt.Println("channel sleeping for 2 seconds")
+	time.Sleep(2 * time.Second)
+	fmt.Println("channel awake")
+
+	channelCall <- data
+	fmt.Println("channel message send :", data)
 }
 
 func main() {
-	var j  = 1
-	j ++
-	for i := 0; i < 3; i ++ {
-		go print(i)
-	}
+	//var j  = 1
+	//j ++
+	//for i := 0; i < 3; i ++ {
+	//	go print(i)
+	//}
+	//
+	//var input string
+	//fmt.Scanln(&input)
 
-	var input string
-	fmt.Scanln(&input)
+	// channel
+	data := "one percent"
+	channelCall := make(chan string)
+
+	go hello(data, channelCall)
+
+	receivedData := <- channelCall
+
+	fmt.Println("received message data :", receivedData)
 
 }
