30cdec2d24d487b7079c14756f6ae6750e5886f9
yijun.liu <francislyj@gmail.com>
2021-07-12 18:56:46 +0800

    file

--- file_control.go
+++ file_control.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	//file, err := os.Create("hello.txt")
+	//
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//fmt.Println("file created success", file)
+
+	file, err := os.Open("hello.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file.WriteString("hello world")
+
+	defer file.Close()
+	fmt.Println("file written success")
+}
