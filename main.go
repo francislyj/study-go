f437059b9a50fa8bcb8b720c3b499a8fa20032c0
yijun.liu <francislyj@gmail.com>
2021-07-12 11:11:08 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func Comment() {
+	// this is a single line comment
+	/*
+	this is a multi line comment
+	*/
+}
+
+func DataTypes() {
+	// Declaring and Initializing Constant
+	const con1 = 1
+	const con2 = 15
+
+	// Declaring and Initializing Variable
+	var var1 = 5
+	var var2 = 12
+
+	// Changing Variable value
+	var1 = 25
+	var2 = 35
+
+	// Print Value
+	fmt.Printf("con1 is %d \n", con1)
+	fmt.Printf("con2 is %d \n", con2)
+	fmt.Printf("var1 is %d \n", var1)
+	fmt.Printf("con2 is %d \n", var2)
+
+}
+
+func Constants()  {
+
+}
+
+func main() {
+	DataTypes()
+}
